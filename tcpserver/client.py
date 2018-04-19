bfecc7a6b6ca91d6cf4c9ab44610e7ce9dad3aaa
baixue <baixuexue123@hotmail.com>
2018-04-20 00:32:02 +0800

    tcpserver test

---
+++ tcpserver/client.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import logging
+from time import time
+
+from tornado import gen
+from tornado.ioloop import IOLoop
+from tornado.iostream import StreamClosedError
+from tornado.options import options, define
+from tornado.tcpclient import TCPClient
+
+define("host", default="localhost", help="TCP server host")
+define("port", default=9888, help="TCP port to connect to")
+define("message", default="ping", help="Message to send")
+
+logger = logging.getLogger(__name__)
+
+
+@gen.coroutine
+def send_message(flag):
+    stream = yield TCPClient().connect(options.host, options.port)
+    while 1:
+        message = options.message + ': %s : %s\n' % (time(), flag)
+        try:
+            yield stream.write(message.encode())
+            print("Sent to server:", options.message)
+            reply = yield stream.read_until(b"\n")
+            print("Response from server:", reply.decode().strip())
+        except StreamClosedError:
+            logger.warning("Lost connection")
+            break
+        except Exception as e:
+            print(e)
+
+        yield gen.sleep(0.5)
+
+
+if __name__ == "__main__":
+    options.parse_command_line()
+
+    io_loop = IOLoop.current()
+
+    for i in range(100):
+        io_loop.add_future(send_message(i), callback=lambda f: None)
+
+    io_loop.start()
---
+++ tcpserver/demo1.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import logging
+
+from tornado import gen
+from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado.tcpserver import TCPServer
+from tornado.iostream import StreamClosedError
+from tornado.options import define, options, parse_command_line
+
+define("port", default=9888, help="TCP port to listen on")
+
+logger = logging.getLogger(__name__)
+
+
+class EchoServer(TCPServer):
+    @gen.coroutine
+    def handle_stream(self, stream, address):
+        while 1:
+            try:
+                data = yield stream.read_until(b"\n")
+                logger.info("Received bytes: %s", data)
+                if not data.endswith(b"\n"):
+                    data = data + b"\n"
+                yield stream.write(data)
+            except StreamClosedError:
+                logger.warning("Lost client at host %s", address[0])
+                break
+            except Exception as e:
+                print(e)
+
+def main():
+    parse_command_line()
+
+    server = EchoServer()
+    server.listen(options.port)
+    logger.info("Listening on TCP port %d", options.port)
+    io_loop = IOLoop.current()
+
+    def callback():
+        print('---------------------')
+        print(len(io_loop.handlers))
+
+    period = PeriodicCallback(callback=callback, callback_time=500)
+    period.start()
+
+    io_loop.start()
+
+
+if __name__ == "__main__":
+    main()
