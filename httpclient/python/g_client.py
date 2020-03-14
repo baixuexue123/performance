188a3403a9049f4b532867940be154f8566a3f11
baixue <pysaoke@gmail.com>
2020-03-14 21:31:24 +0800

    httpclient

---
+++ httpclient/python/g_client.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import gevent
+import gevent.monkey; gevent.monkey.patch_socket()
+
+import time
+import requests
+
+urls = [
+    'http://www.sohu.com',
+    'http://www.sina.com',
+    'http://www.qq.com/',
+    'http://www.zhaopin.com/',
+    'http://www.jd.com/',
+    'http://www.zhibo8.cc/',
+    'http://www.iqiyi.com/',
+    'http://www.bootcss.com/',
+    'http://www.redis.cn/',
+    'http://cnodejs.org/',
+    'http://bbs.tianya.cn/',
+    'http://spark.apache.org/',
+]
+
+
+def http_get(url):
+    st = time.time()
+    resp = requests.get(url)
+    return url, resp, time.time() - st
+
+
+def main():
+    threads = []
+    for url in urls:
+        threads.append(gevent.spawn(http_get, url))
+    gevent.joinall(threads)
+
+
+if __name__ == '__main__':
+    st = time.time()
+    main()
+    print(time.time() - st)
